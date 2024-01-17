14955375839aa97e44cc4c7a702ae8b081f85727
kai <kylestech95@outlook.com>
2024-01-17 10:22:09 -0500

    fix: validate-url

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const bodyParser = require('body-parser')
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
@@ -14,9 +15,22 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+// test valid url
+const testValidURL = (url) => {
+  const regex = new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/)
+  return regex.test(url)
+}
+// post valid url
+app.post('/api/shorturl', function(req, res) {
+  let url = req.body.url
+  console.log(url)
+  if(testValidURL(url)){
+    return res.json({original_url:url})
+  }
+  else{
+    return res.json({ error: 'invalid url' });
+  }
+  
 });
 
 app.listen(port, function() {
